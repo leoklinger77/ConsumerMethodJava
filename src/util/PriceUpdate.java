a4c9b53a3b7c9ff7b6449bb154bb16341319b9b5
Leandro Klinger <Leandro.klingeroliveira@gmail.com>
2020-07-21 23:34:02 -0300

    Adding the Clase PriceUpdate method

--- src/util/PriceUpdate.java
+++ src/util/PriceUpdate.java
@@ -4,12 +4,11 @@ import java.util.function.Consumer;
 
 import entities.Product;
 
-public class PriceUpdate implements Consumer<Product>{
+public class PriceUpdate implements Consumer<Product> {
 
 	@Override
 	public void accept(Product p) {
-		// TODO Auto-generated method stub
-		
+		p.setPrice(p.getPrice() * 1.1);
 	}
 
 }
