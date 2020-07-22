a543ea1e803e7c8e29dd5d6fbedee629109cbdf4
Leandro Klinger <Leandro.klingeroliveira@gmail.com>
2020-07-21 23:31:06 -0300

    PriceUpdate Class Created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,24 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		//decrease the price by 10 percent
+		
+		list.forEach(action);
+		
 	}
 
 }
