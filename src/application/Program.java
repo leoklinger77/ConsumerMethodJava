f514484452b03a7a7082a516befa0866ff256b35
Leandro Klinger <Leandro.klingeroliveira@gmail.com>
2020-07-21 23:36:16 -0300

    Updating Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,21 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 import entities.Product;
+import util.PriceUpdate;
 
 public class Program {
 
 	public static void main(String[] args) {
 		List<Product> list = new ArrayList<>();
-		
+
 		list.add(new Product("Tv", 900.00));
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
-		
-		//decrease the price by 10 percent
-		
-		list.forEach(action);
-		
+
+		// decrease the price by 10 percent
+
+		list.forEach(new PriceUpdate());
+
+		for (Product p : list) {
+			System.out.println(p);
+		}
+
 	}
 
 }
